482799164e9608cd473b768806e3cb010d7e423e
chenzheio <github@chenzhe.dev>
2023-03-01 17:55:32 +0800

    Refactor code for improved readability in main.ts

--- src/main.ts
+++ src/main.ts
@@ -61,10 +61,7 @@ import { readFileSync, writeFileSync } from "fs";
     }, new NewMessage({ outgoing: true }));
 
     client.addEventHandler(async ({ message: _M }: NewMessageEvent) => {
-
         console.log(_M.message);
-
-
     }, new NewMessage({ outgoing: false }));
 
 })();
